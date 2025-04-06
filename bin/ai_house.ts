ab9bddcaddc98ffd48126155412508c92b6ad80e
tanaya1307 <placetocalltanaya@gmail.com>
2025-04-06 18:01:34 +0530

    combined all the stacks into one

--- bin/ai_house.ts
+++ bin/ai_house.ts
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { UnifiedStack } from './unifiedStack';
+import { CombinedInfraStack } from '../lib/stacks/combined-infra-stack';
+
+
 
 const app = new cdk.App();
-
-new UnifiedStack(app, 'UnifiedSaaSStack', {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
+new CombinedInfraStack(app, 'CombinedInfraStack', {
+  env: { 
+    account: process.env.CDK_DEFAULT_ACCOUNT || 'default-account', 
+    region: process.env.CDK_DEFAULT_REGION || 'default-region' 
   },
 });
-
 app.synth();
